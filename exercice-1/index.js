72078561f1c73c6b035b4b060faadc0f7cda41e5
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-16 10:09:39 +0100

    exo 1

---
+++ exercice-1/index.js
@@ -0,0 +1,5 @@
+import { question } from "./variables.js"
+import { answer } from "./variables.js" 
+window.addEventListener("DOMContentLoaded", () => {
+     console.log(question, answer);
+});
---
+++ exercice-1/variables.js
@@ -0,0 +1,4 @@
+export let question = "Life, the universe and everything in it"
+
+export const answer = 42;
+
