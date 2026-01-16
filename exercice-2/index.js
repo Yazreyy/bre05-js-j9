b62d56b4534ce595a0d1c539eb652e2be6905f3c
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-16 10:17:23 +0100

    exercice 2

---
+++ exercice-2/index.js
@@ -0,0 +1,7 @@
+import { numbers } from "./numbers.js"
+
+window.addEventListener("DOMContentLoaded", () => {
+   for(let i = 0 ; i < numbers.length ; i++){
+     console.log(numbers[i]);
+   }
+});
